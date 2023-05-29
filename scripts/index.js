a9a88add05f17917a98965e67a3d012af9e98772
Bùi Duy <buiduy@Mac-mini-cua-Bui.local>
2023-05-29 13:12:14 +0700

    add dropdown menu

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+// for dropdown menu
+const dropdownEls = document.querySelectorAll(".dropdown");
+
+dropdownEls.forEach((el) => {
+  el.addEventListener("mouseover", () => {
+    el.classList.add("drop");
+  });
+});
+
+dropdownEls.forEach((el) => {
+  el.addEventListener("mouseout", () => {
+    el.classList.remove("drop");
+  });
+});
