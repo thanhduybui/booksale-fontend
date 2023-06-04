253be25199c4f5400cb566d7ae3832a5eff7d138
Bùi Duy <buiduy@Mac-mini-cua-Bui.local>
2023-06-04 09:14:21 +0700

    done slider section

--- scripts/index.js
+++ scripts/index.js
@@ -20,3 +20,76 @@ btnMobile.addEventListener("click", () => {
   const nav = document.querySelector(".navigation").parentElement;
   nav.classList.toggle("nav-open");
 });
+
+// slider
+const sliderHandler = function () {
+  const slides = document.querySelectorAll(".slide");
+  const btnPrev = document.querySelector(".btn-left");
+  const btnNext = document.querySelector(".btn-right");
+  const dotContainer = document.querySelector(".dots");
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  // Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<div class="dot" data-slide="${i}"></div>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll(".dot")
+      .forEach((dot) => dot.classList.remove("dot--current"));
+
+    document
+      .querySelector(`.dot[data-slide="${slide}"]`)
+      .classList.add("dot--current");
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+
+    activateDot(0);
+  };
+  init();
+
+  // Event handlers
+  btnNext.addEventListener("click", nextSlide);
+  btnPrev.addEventListener("click", prevSlide);
+};
+
+sliderHandler();
