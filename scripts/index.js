fcf979f0e5ddfe6f353b7397df106ede67bcb8eb
Bùi Thanh Duy <dtb1742002@gmail.com>
2023-06-26 12:36:49 +0700

    retrieve data from server

--- scripts/index.js
+++ scripts/index.js
@@ -112,3 +112,93 @@ const sliderHandler = function () {
 };
 
 sliderHandler();
+
+// Load data books based on url
+
+const booksLinkEl = document.querySelectorAll(".bs-link");
+
+booksLinkEl.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const url = event.target.getAttribute("href");
+    const container = link
+      .closest(".bookshelf")
+      .querySelector(".books-container");
+    loadDataBooks(url, container);
+  });
+});
+
+//remove book cards
+const removeBookCards = () => {
+  const bookCards = document.querySelectorAll(".book-card");
+
+  bookCards.forEach((card) => {
+    card.remove();
+  });
+};
+
+function loadDataBooks(url, container) {
+  console.log(container);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // Update the booksContainer with the retrieved data
+      const insertHtml = data.map((book) => renderCard(book)).join("");
+      removeBookCards();
+      container.insertAdjacentHTML("beforeend", insertHtml);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function renderCard(book) {
+  return `
+  <div class="book-card">
+  <span class="discount-tag">-${book.discount}%</span>
+  <img
+    src=${book.mainImg}
+    alt="Book Cover"
+    class="card-img"
+  />
+  <div class="card-body">
+    <h3 class="card-title">${titleHanlder(book.title)}</h3>
+    <div class="rating card-rating">
+      <span class="rating-number">4.8</span
+      ><span><ion-icon name="star"></ion-icon></span
+      ><span class="rating-text">Đã bán 200</span>
+    </div>
+    <div class="card-prices">
+      <span class="card-discount">${book.finalPrice}đ</span>
+      <span class="card-price">${book.price}đ</span>
+    </div>
+    <a href="#" class="card-link">mua sách &rarr;</a>
+  </div>
+</div>`;
+}
+
+function titleHanlder(title) {
+  let result = title;
+  if (title.length > 50) {
+    result = title.substring(0, 50) + "...";
+  }
+  return result;
+}
+
+// ----------------------------------------------------------------
+// HANDLE TAB ON CATEGORY BOOK
+const categoryLinkEl = document.querySelectorAll(".bs-link");
+
+categoryLinkEl.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    removeSelectedLinks();
+    event.target.parentElement.classList.add("selected");
+  });
+});
+
+// remove current selected link
+function removeSelectedLinks() {
+  categoryLinkEl.forEach((link) => {
+    link.parentElement.classList.remove("selected");
+  });
+}
